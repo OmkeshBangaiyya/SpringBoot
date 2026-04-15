29fc3cbdebff0eff18f3203f0354983a34769cb8
omkesh <omkeshbangaiyya18@gmail.com>
2026-04-16 00:18:22 +0530

    manually constructor injection by using applictionConstructorInjection.xml file

---
+++ SpringExample/src/main/java/car/example/constructor/injection/Specification.java
@@ -0,0 +1,32 @@
+package car.example.constructor.injection;
+
+public class Specification {
+    private String make;
+    private String model;
+
+    //getter
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    //setter
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
